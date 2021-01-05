461cd9866af5895964f38f19c520d041deab24ba
WilhanJovi <76637374+WilhanJovi@users.noreply.github.com>
2021-01-05 11:12:40 +0700

    Update liff-starter.js

--- miyamku/liff-starter.js
+++ miyamku/liff-starter.js
@@ -173,7 +173,7 @@ function registerButtonHandlers() {
         } else {
             liff.sendMessages([{
                 'type': 'text',
-                'text': "Terimakasih ${namaLine}, sudah memesan "+_text
+                'text': "Terimakasih ${namaLine}, sudah memesan makanan"
             }]).then(function() {
                 window.alert('Ini adalah pesan dari fitur Send Message');
             }).catch(function(error) {
